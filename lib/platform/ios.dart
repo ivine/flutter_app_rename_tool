71be186da9724482946cc3db675207e66efd8890
dengwww_c1 <vinzry@163.com>
2025-03-24 15:38:16 +0800

    feat: ios rename func move to darwin_util

--- lib/platform/ios.dart
+++ lib/platform/ios.dart
@@ -1,9 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
-
-import 'dart:io';
-
 import 'package:yaml/yaml.dart';
-import 'package:xcode_parser1/xcode_parser1.dart';
 
 import '../const/const.dart';
 import '../const/settings.dart';
@@ -11,9 +7,9 @@ import '../util/log_util.dart';
 import '../util/darwin_util.dart';
 
 class FARPlatformIOS {
+  String platformName = 'ios';
   String bundleDisplayName = '';
   String bundleName = '';
-
   List<DarwinBundleIDSettings> bundleIdSettings = [];
 
   late String currentDirPath;
@@ -47,9 +43,6 @@ class FARPlatformIOS {
       bundleName = '';
     }
 
-    // 更新 Info.plist 文件内容
-    _update_plistFileName();
-
     // get bundle id
     dynamic bundleId = iosSettings[keyDarwinBundleId] ?? '';
     if (bundleId is YamlMap) {
@@ -66,76 +59,21 @@ class FARPlatformIOS {
       ];
     }
 
+    // 更新 Info.plist 中的 `CFBundleName` 和 `CFBundleDisplayName`
+    DarwinUtil.updatePlistFileName(
+      dir: currentDirPath,
+      platformName: platformName,
+      bundleName: bundleName,
+      bundleDisplayName: bundleDisplayName,
+    );
+
     // 更新 bundle id
-    await _update_pbxproj_bundleId();
+    DarwinUtil.updatePbxprojBundleId(
+      dir: currentDirPath,
+      platformName: platformName,
+      bundleIdSettings: bundleIdSettings,
+    );
 
     log("iOS app name update completed. ✅");
-  }
-
-  // 更新 Info.plist 中的 `CFBundleName` 和 `CFBundleDisplayName`
-  void _update_plistFileName() {
-    try {
-      final file = File('$currentDirPath/ios/Runner/Info.plist');
-      if (!file.existsSync()) {
-        log("iOS info.plist does not exist at ${file.path}");
-        return;
-      }
-      final content = file.readAsStringSync();
-      final updatedContent = DarwinUtil.replacePlistFields(
-        content,
-        {
-          'CFBundleName': bundleName,
-          'CFBundleDisplayName': bundleDisplayName,
-        },
-      );
-
-      file.writeAsStringSync(updatedContent, flush: true);
-    } catch (e) {
-      log("iOS error updating Info.plist - $e");
-    }
-  }
-
-  // 更新 bundle id
-  Future _update_pbxproj_bundleId() async {
-    try {
-      String filePath = '$currentDirPath/ios/$fileNameRunnerPbxproj';
-      Pbxproj project = await Pbxproj.open(filePath);
-      final object = project.find("objects") as MapPbx;
-      final configList = object.find("XCConfigurationList") as SectionPbx;
-      final buildConfig = object.find("XCBuildConfiguration") as SectionPbx;
-      final nativeTarget = configList.childrenList
-          .where((element) {
-            String tmpComment = element.comment ?? "";
-            return tmpComment.contains("PBXNativeTarget") && tmpComment.contains("\"Runner\"");
-          })
-          .toList()
-          .first;
-      final buildConfigurations = (nativeTarget as MapPbx).find("buildConfigurations") as dynamic;
-      List<String> uuids = [];
-      for (var i = 0; i < (buildConfigurations as ListPbx).length; i++) {
-        final e = buildConfigurations[i] as ElementOfListPbx;
-        uuids.add(e.value);
-      }
-      final targetBuildConfigs = buildConfig.childrenList.where((e) => uuids.contains(e.uuid)).whereType<MapPbx>().toList();
-      for (final mp in targetBuildConfigs) {
-        _updateBundleIdForConfiguration(mp);
-      }
-      await DarwinUtil.writeContentToProjectFile(filePath, project);
-    } catch (e) {
-      log("iOS update .pbxproj file fail, error: $e");
-    }
-  }
-
-  void _updateBundleIdForConfiguration(MapPbx mp) {
-    String buildTypeString = mp.comment?.toLowerCase() ?? "";
-    if (buildTypeString.isEmpty) {
-      return;
-    }
-    List<DarwinBundleIDSettings> settings = bundleIdSettings.where((element) => element.buildType == buildTypeString).toList();
-    if (settings.isEmpty) {
-      return;
-    }
-    String bundleId = settings.first.bundleId;
-    DarwinUtil.updateBundleIdForConfiguration(mp, buildTypeString, bundleId);
   }
 }
--- lib/util/darwin_util.dart
+++ lib/util/darwin_util.dart
@@ -2,9 +2,41 @@ import 'dart:io';
 
 import 'package:xcode_parser1/xcode_parser1.dart';
 
+import '../const/const.dart';
+import '../const/settings.dart';
+import 'log_util.dart';
+
 class DarwinUtil {
+  // 更新 Info.plist 中的 `CFBundleName` 和 `CFBundleDisplayName`
+  static void updatePlistFileName({
+    required String dir,
+    required String platformName,
+    required String bundleName,
+    required String bundleDisplayName,
+  }) {
+    try {
+      final file = File('$dir/$platformName/Runner/Info.plist');
+      if (!file.existsSync()) {
+        log("iOS info.plist does not exist at ${file.path}");
+        return;
+      }
+      final content = file.readAsStringSync();
+      final updatedContent = _replacePlistFields(
+        content,
+        {
+          'CFBundleName': bundleName,
+          'CFBundleDisplayName': bundleDisplayName,
+        },
+      );
+
+      file.writeAsStringSync(updatedContent, flush: true);
+    } catch (e) {
+      log("iOS error updating Info.plist - $e");
+    }
+  }
+
   // 替换指定字段的值
-  static String replacePlistFields(String content, Map<String, String> replacements) {
+  static String _replacePlistFields(String content, Map<String, String> replacements) {
     var updatedContent = content;
     replacements.forEach((key, value) {
       final regex = RegExp('<key>$key</key>\\s*<string>.*?</string>');
@@ -18,7 +50,65 @@ class DarwinUtil {
     return updatedContent;
   }
 
-  static Future<void> writeContentToProjectFile(String filePath, Pbxproj project) async {
+  // platformName: ios or macos
+  static Future updatePbxprojBundleId({
+    required String dir,
+    required String platformName,
+    required List<DarwinBundleIDSettings> bundleIdSettings,
+  }) async {
+    try {
+      String filePath = '$dir/$platformName/$fileNameRunnerPbxproj';
+      Pbxproj project = await Pbxproj.open(filePath);
+      final configList = project.find("XCConfigurationList") as SectionPbx;
+      final buildConfig = project.find("XCBuildConfiguration") as SectionPbx;
+      final nativeTarget = configList.childrenList
+          .where((element) {
+            String tmpComment = element.comment ?? "";
+            return tmpComment.contains("PBXNativeTarget") && tmpComment.contains("\"Runner\"");
+          })
+          .toList()
+          .first;
+      final buildConfigurations = (nativeTarget as MapPbx).find("buildConfigurations") as dynamic;
+      List<String> uuids = [];
+      for (var i = 0; i < (buildConfigurations as ListPbx).length; i++) {
+        final e = buildConfigurations[i] as ElementOfListPbx;
+        uuids.add(e.value);
+      }
+      final targetBuildConfigs = buildConfig.childrenList.where((e) => uuids.contains(e.uuid)).whereType<MapPbx>().toList();
+      for (final mp in targetBuildConfigs) {
+        String buildTypeString = mp.comment?.toLowerCase() ?? "";
+        if (buildTypeString.isEmpty) {
+          return;
+        }
+        List<DarwinBundleIDSettings> settings = bundleIdSettings.where((element) => element.buildType == buildTypeString).toList();
+        if (settings.isEmpty) {
+          return;
+        }
+        String bundleId = settings.first.bundleId;
+        _updateBundleIdForConfiguration(mp, buildTypeString, bundleId);
+      }
+      await _writeContentToProjectFile(filePath, project);
+    } catch (e) {
+      log("$platformName update .pbxproj file fail, error: $e");
+    }
+  }
+
+  // type: Debug/Profile/Release...
+  static void _updateBundleIdForConfiguration(MapPbx mp, String buildType, String bundleId) {
+    if (mp.comment != buildType) {
+      return;
+    }
+    final buildSettings = mp.find<MapPbx>("buildSettings")!;
+    final bundleIdEntry = buildSettings.find<MapEntryPbx>("PRODUCT_BUNDLE_IDENTIFIER")!;
+    final bundleValue = bundleIdEntry.value as VarPbx;
+    buildSettings.replaceOrAdd(
+      bundleIdEntry.copyWith(
+        value: bundleValue.copyWith(value: bundleId),
+      ),
+    );
+  }
+
+  static Future<void> _writeContentToProjectFile(String filePath, Pbxproj project) async {
     final file = File(filePath);
     if (!await file.exists()) {
       await file.create();
@@ -39,20 +129,5 @@ class DarwinUtil {
     String projectString = project.formatOutput(project.toString());
     projectString = removeMultipleUTF8String(projectString);
     file.writeAsStringSync(projectString);
-  }
-
-  // type: Debug/Profile/Release...
-  static void updateBundleIdForConfiguration(MapPbx mp, String buildType, String bundleId) {
-    if (mp.comment != buildType) {
-      return;
-    }
-    final buildSettings = mp.find<MapPbx>("buildSettings")!;
-    final bundleIdEntry = buildSettings.find<MapEntryPbx>("PRODUCT_BUNDLE_IDENTIFIER")!;
-    final bundleValue = bundleIdEntry.value as VarPbx;
-    buildSettings.replaceOrAdd(
-      bundleIdEntry.copyWith(
-        value: bundleValue.copyWith(value: bundleId),
-      ),
-    );
   }
 }
